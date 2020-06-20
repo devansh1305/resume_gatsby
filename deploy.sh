261af38bfabf306be7933d3b125aed1742344a20
Devansh Panirwala <dpanirwa@purdue.edu>
2020-06-20 09:38:06 -0400

    minor changes

--- deploy.sh
+++ deploy.sh
@@ -4,4 +4,4 @@ gatsby clean && gatsby build --prefix-paths
 sshpass -e ssh -o StrictHostKeyChecking=no "$pu_server" "rm -r .www/*"
 sshpass -e scp -o StrictHostKeyChecking=no -r ./public/* "$pu_server_www"
 sshpass -e ssh -o StrictHostKeyChecking=no "$pu_server" "cd .www;chmod -R 755 ./*"
-cat ~/myunsetenv.sh
+source ~/myunsetenv.sh
